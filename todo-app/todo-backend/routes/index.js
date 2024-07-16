276251ebd28b1602386db657306dc5e24f6d7dfe
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-07-16 22:30:33 +0700

    re-setup backend

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const configs = require('../util/config')
+
+let visits = 0
+
+/* GET index data. */
+router.get('/', async (req, res) => {
+  visits++
+
+  res.send({
+    ...configs,
+    visits
+  });
 });
 
 module.exports = router;
